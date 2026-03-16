ffab9b4ec2a27203924122b7fe61041cd9261177
ramlokesh59 <ramlokesh59@gmail.com>
2026-03-16 15:05:20 +0530

    Initial commit - Book My Stay App

---
+++ src/src/BookMyStayApp.java
@@ -0,0 +1,17 @@
+
+public class BookMyStayApp {
+
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to the Hotel Booking System!");
+
+
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: 1.0");
+
+        // Inform user that application is starting
+        System.out.println("System initialized successfully.");
+    }
+}
